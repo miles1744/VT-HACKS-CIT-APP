e14d8eff6f02faf5d86dd041c25d011866331026
shubhasmishra <shubham21@vt.edu>
2024-09-14 11:19:43 -0400

    push

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 const Perspective = require('perspective-api-client');
 
 Perspective.discoverAPI()
+
+require('dotenv').config();
+const axios = require('axios');
+
+const api_key_link = process.env.DISCORD_API_KEY
+
+const {google} = require('googleapis');
+
+async function analyzeKey(text){
+    const url = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=' + api_key_link;
+}
+
+
+google.discoverAPI(DISCOVERY_URL)
+    .then(client => {
+      const analyzeRequest = {
+        comment: {
+          text: 'Jiminy cricket! Well gosh durned it! Oh damn it all!',
+        },
+        requestedAttributes: {
+          TOXICITY: {},
+        },
+      };
+
+      client.comments.analyze(
+          {
+            key: API_KEY,
+            resource: analyzeRequest,
+          },
+          (err, response) => {
+            if (err) throw err;
+            console.log(JSON.stringify(response.data, null, 2));
+          });
+    })
+    .catch(err => {
+      throw err;
+    });
