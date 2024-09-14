b394cc558443ebacd5250d0ec105f83031d67020
shubhasmishra <shubham21@vt.edu>
2024-09-14 18:16:50 -0400

    update app.js

--- discord-bot/app.js
+++ discord-bot/app.js
@@ -64,16 +64,6 @@ client.on('messageCreate', async (message) => {
     await sendMessageWithDelay(message.channel,`Hey, ${message.author}!`);
   }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  if (message.content === 'miles'){
-    await sendMessageWithDelay(message.channel,'miles i like ur gyatt - discord');
-  }
-=======
->>>>>>> Stashed changes
-
-=======
->>>>>>> Stashed changes
   const content = message.content;
   const scores = await analyzeMessage(content);
 
