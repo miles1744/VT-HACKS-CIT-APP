c4c7de9abc63a5f8f2b9a3a8d51f81eba80b3e9f
shubhasmishra <shubham21@vt.edu>
2024-09-14 10:08:10 -0400

    API

--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -8,3 +8,34 @@ const client = new Client({
         IntentsBitField.Flags.MessageContent
     ]
 })
+
+const {google} = require('googleapis');
+
+API_KEY = '';
+DISCOVERY_URL =
+    'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1';
+
+google.discoverAPI(DISCOVERY_URL)
+    .then(client => {
+      const analyzeRequest = {
+        comment: {
+          text: 'Jiminy cricket! Well gosh durned it! Oh damn it all!',
+        },
+        requestedAttributes: {
+          TOXICITY: {},
+        },
+      };
+
+      client.comments.analyze(
+          {
+            key: API_KEY,
+            resource: analyzeRequest,
+          },
+          (err, response) => {
+            if (err) throw err;
+            console.log(JSON.stringify(response.data, null, 2));
+          });
+    })
+    .catch(err => {
+      throw err;
+    });
